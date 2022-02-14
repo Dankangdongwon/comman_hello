40f0bcfd9bd78d21c0dce9586b7320f92381203a
Daniel <kwcs99@gmail.com>
2022-02-14 13:56:21 +0900

    Local comment cahracter modified

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
-// For command line git tutorial
-// First: GitHub modification
-// Second: Local repository modification
+# For command line git tutorial
+# First: GitHub modification
+# Second: Local repository modification
 
 print("Hello World")
 print("Tell your world")
